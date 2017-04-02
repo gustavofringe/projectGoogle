5fade219b259bb6c2fb8f8100983382e4fa7f248
Dussart guillaume <dussartguillaume@orange.fr>
2017-04-02 11:49:53 +0200

    change js for display params menu

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-$(document).ready(function() {
+(function($) {
     $('.params').click(function(e) {
-        $('.dropdown-menu').toggle();
+        $('.dropdown-menu').fadeToggle(200);
     });
-    $('html').click(function(e) {
+/*    $('#fa-th').click(function(e) {
         $('#appli').toggle();
-    });
-});
+    });*/
+    function toggle_visibility(id) {
+        var e = document.getElementById('appli');
+        if(e.style.display == 'block')
+            e.style.display = 'none';
+        else
+            e.style.display = 'block';
+    }
+})(jQuery);
