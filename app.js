d9bd5b2989c6dbfbdb1b7ed199a5bf9e3e2f6bce
Dussart guillaume <dussartguillaume@orange.fr>
2017-04-02 11:56:38 +0200

    change js for appli

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-(function($) {
-    $('.params').click(function(e) {
+(function ($) {
+    $('.params').click(function (e) {
         $('.dropdown-menu').fadeToggle(200);
     });
-/*    $('#fa-th').click(function(e) {
+    $('#fa-th').click(function (e) {
         $('#appli').toggle();
-    });*/
-    function toggle_visibility(id) {
-        var e = document.getElementById('appli');
-        if(e.style.display == 'block')
-            e.style.display = 'none';
-        else
-            e.style.display = 'block';
-    }
+    });
+
 })(jQuery);
